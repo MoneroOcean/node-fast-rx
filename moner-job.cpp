13677ea9c0990159c46f0dd7736317034f656249
MoneroOcean <support@moneroocean.stream>
2025-07-09 18:54:28 -0700

    Fixed race condition

--- moner-job.cpp
+++ moner-job.cpp
@@ -308,8 +308,10 @@ void Core::set_job(
   // start rx job compute threads
   if (new_dev == DEV::RX_CPU) {
     const unsigned job_ref = m_job_ref;
+    std::atomic<int> unique_counter{0};
     for (unsigned batch_id = 0; batch_id != m_batch; ++batch_id) m_thread_pool->push(
-      [=, &m_job_ref = m_job_ref, &m_hash_count = m_hash_count](int thread_id) {
+      [=, &m_job_ref = m_job_ref, &m_hash_count = m_hash_count, &unique_counter](int) {
+        const int thread_id = unique_counter.fetch_add(1);
         try {
           alignas(16) uint8_t  input[MAX_BLOB_LEN];
           alignas(16) uint8_t  output[HASH_LEN];
